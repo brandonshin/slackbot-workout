c87b7eed11e3341d2af5bd94ef2452e61b3dc01c
Miles Yucht <miles@databricks.com>
2015-12-27 14:00:02 -0800

    Update sample

--- sample_db.py
+++ sample_db.py
@@ -1,40 +1,16 @@
-from bot.bot import Bot
 from logger.loggers import PostgresDatabaseLogger
-from util.configurators import EnvironmentTokenProvider, JsonFileConfigurationProvider
-import time
+from util.configurators import InMemoryTokenProvider, JsonFileConfigurationProvider
+from server.server import Server
 import os
 
 def main():
     logger = PostgresDatabaseLogger('flexecution', 'flexecution',
             host='flexecution-postgres.chlee7xx28jo.us-west-2.rds.amazonaws.com', port=5432,
-            user='flexecution', password='flexecution')
-    configuration = JsonFileConfigurationProvider(os.getcwd() + '/config.json')
-    tokens = EnvironmentTokenProvider()
-    bot = Bot(logger, configuration, tokens)
+            user='flexecution', password='DcHqIxAE^IbgX52K9!5R')
+    config = JsonFileConfigurationProvider(os.getcwd() + '/config.json')
+    tokens = InMemoryTokenProvider('xoxp-2499669547-10707224704-15892141524-4b7e5a03b7')
+    server = Server(logger, config, tokens)
+    server.start()
 
-    try:
-        while True:
-            if bot.isOfficeHours():
-                # Re-fetch config file if settings have changed
-                bot.loadConfiguration()
-
-                # Get an exercise to do
-                exercise = bot.selectExerciseAndStartTime()
-
-                # Assign the exercise to someone
-                bot.assignExercise(exercise)
-
-            else:
-                # Sleep the script and check again for office hours
-                if not bot.debug:
-                    time.sleep(5*60) # Sleep 5 minutes
-                else:
-                    # If debugging, check again in 5 seconds
-                    time.sleep(5)
-
-    except KeyboardInterrupt:
-        bot.printStats()
-        bot.saveUsers()
-
-
-main()
+if __name__ == "__main__":
+    main()

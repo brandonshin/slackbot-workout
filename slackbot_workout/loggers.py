154d53a9862b0e9ae7fef4ea264b36103fbe77c1
Miles Yucht <mgyucht@gmail.com>
2015-12-31 17:54:10 -0800

    Add functionality to check user's current exercises

--- slackbot_workout/loggers.py
+++ slackbot_workout/loggers.py
@@ -11,30 +11,50 @@ class BaseLogger:
     def log_exercise(self, username, exercise, reps, units):
         pass
 
+    @abstractmethod
+    def get_todays_exercises(self):
+        pass
+
 class StdOutLogger(BaseLogger):
     def log_exercise(self, username, exercise, reps, units):
         print "%s %s %d %s" % (username, exercise, reps, units)
 
+    def get_todays_exercises(self):
+        # We aren't actually persisting this data, so return no exercises for anyone
+        return {}
+
 class CsvLogger(BaseLogger):
-    def __init__(self, debug):
+    format_string = "%Y%m%d"
+
+    def __init__(self, debug=False):
         self.debug = debug
-        debugString = "_DEBUG" if debug else ""
-        logfilename = "log" + time.strftime("%Y%m%d-%H%M")
-        self.csv_filename = logfilename + debugString + ".csv" if debug else logfilename + ".csv"
+
+    def csv_filename(self):
+        debugString = "_DEBUG" if self.debug else ""
+        return "log" + time.strftime(self.format_string) + debugString + ".csv"
 
     def log_exercise(self, username, exercise, reps, units):
-        with open(self.csv_filename, 'a') as f:
+        with open(self.csv_filename(), 'a') as f:
             writer = csv.writer(f)
-            writer.writerow([str(datetime.datetime.now()),username,exercise,reps,units,self.debug])
+            writer.writerow([str(datetime.datetime.now()),username,exercise,reps,units])
 
-class PostgresDatabaseLogger(BaseLogger):
-    def __init__(self, dbname, tablename, **kwargs):
-        self.debug = 'debug' in kwargs and kwargs['debug'] == True
-        self.kwargs = kwargs
-        self.kwargs.update({'dbname': dbname})
-        self.tablename = tablename
-        self.maybe_create_database()
+    def get_todays_exercises(self):
+        exercises = {}
+        with open(self.csv_filename(), 'r') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                username = row[1]
+                exercise_data = {
+                    'exercise': row[2],
+                    'reps': int(row[3])
+                }
+                try:
+                    exercises[username].append(exercise_data)
+                except:
+                    exercises[username] = [exercise_data]
+        return exercises
 
+class PostgresConnector:
     def with_connection(self, func):
         conn = None
         try:
@@ -42,10 +62,11 @@ class PostgresDatabaseLogger(BaseLogger):
             cursor = conn.cursor()
             if self.debug:
                 print "precursor"
-            func(cursor)
+            result = func(cursor)
             conn.commit()
             if self.debug:
                 print "committed"
+            return result
         except psycopg2.Error as e:
             if self.debug:
                 print e
@@ -54,8 +75,16 @@ class PostgresDatabaseLogger(BaseLogger):
             if conn:
                 conn.close()
 
-    def maybe_create_database(self):
-        def create_database_command(cursor):
+class PostgresDatabaseLogger(BaseLogger, PostgresConnector):
+    def __init__(self, dbname, tablename, **kwargs):
+        self.debug = 'debug' in kwargs and kwargs['debug'] == True
+        self.kwargs = kwargs
+        self.kwargs.update({'dbname': dbname})
+        self.tablename = tablename
+        self.maybe_create_table()
+
+    def maybe_create_table(self):
+        def create_table_command(cursor):
             cursor.execute("""
                 CREATE TABLE IF NOT EXISTS {} (
                     username VARCHAR(100),
@@ -65,7 +94,7 @@ class PostgresDatabaseLogger(BaseLogger):
                     time TIMESTAMP DEFAULT current_timestamp
                 );
             """.format(self.tablename))
-        self.with_connection(create_database_command)
+        self.with_connection(create_table_command)
 
     def log_exercise(self, username, exercise, reps, units):
         def log_exercise_command(cursor):
@@ -77,3 +106,24 @@ class PostgresDatabaseLogger(BaseLogger):
             """.format(self.tablename), (username, exercise, reps, units))
         self.with_connection(log_exercise_command)
 
+    def get_todays_exercises(self):
+        def get_todays_exercises_command(cursor):
+            cursor.execute("""
+                SELECT username, exercise, reps
+                FROM {}
+                WHERE date_trunc('day', time) = date_trunc('day', now())
+            """.format(self.tablename))
+            exercises = {}
+            for row in cursor.fetchall():
+                username = row[0]
+                exercise_data = {
+                    'exercise': row[1],
+                    'reps': row[2]
+                }
+                try:
+                    exercises[username].append(exercise_data)
+                except:
+                    exercises[username] = [exercise_data]
+            return exercises
+
+        return self.with_connection(get_todays_exercises_command)

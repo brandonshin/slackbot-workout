ebf95e3da3ab223cea30d100dbf1f74e20413ca2
Miles Yucht <mgyucht@gmail.com>
2016-01-02 00:57:57 -0800

    Bump version to 0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "slackbot-workout",
-    version = "0.2",
+    version = "0.3",
     packages = find_packages(),
 
     install_requires = [

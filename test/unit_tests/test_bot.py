c723be5004f108e9c61370943ddc6a80ed47866b
Miles Yucht <mgyucht@gmail.com>
2016-01-01 18:08:52 -0800

    fix test

--- test/unit_tests/test_bot.py
+++ test/unit_tests/test_bot.py
@@ -64,10 +64,10 @@ class TestBot(object):
 
     def test_select_next_time_interval(self):
         um, bot = get_sample_bot()
-        um.fetch_active_users.return_value = [
+        eligible_users = [
             User('slackid1', 'username1', 'real name 1'),
             User('slackid2', 'username2', 'real name 2'),
         ]
-        interval = bot.select_next_time_interval()
+        interval = bot.select_next_time_interval(eligible_users)
         assert isinstance(interval, int) or (isinstance(interval, float) and interval.is_integer())
 

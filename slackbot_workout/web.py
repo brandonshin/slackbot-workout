007f19fabe9ad4cf7445b4bfc8b5912f8a719de3
Miles Yucht <mgyucht@gmail.com>
2016-01-01 19:14:36 -0800

    Add slackbot help

--- slackbot_workout/web.py
+++ slackbot_workout/web.py
@@ -24,28 +24,48 @@ class FlexbotWebServer(object):
         user_id = args['user_id']
         text = args['text'].lower()
         if user_id != "USLACKBOT" and self.bot_name in text:
-            words = text.split()
-            user_reverse_lookup = {}
-            users_to_print = []
-            for user_id in self.user_manager.users:
-                user = self.user_manager.users[user_id]
-                user_reverse_lookup[user.username.lower()] = user_id
-                user_reverse_lookup[user.real_name.lower()] = user_id
-            for word in words:
-                if word in user_reverse_lookup:
-                    users_to_print.append(user_reverse_lookup[word])
-                elif word == "channel" or word == "@channel":
-                    users_to_print = self.user_manager.users.keys()
-                    break
-            if len(users_to_print) > 0:
-                stats = self.user_manager.stats(list(set(users_to_print)))
-                self.logger.info("""\
-                vvvvvvvvvvvvvvvvvvvvvvvv
-                responding:
-                {}
-                ^^^^^^^^^^^^^^^^^^^^^^^^""".format(stats))
-                return {
-                    "text": stats
-                }
+            return self.handle_message(text)
+
+    def handle_message(self, text):
+        if "help" in text:
+            return self.print_help()
+        else:
+            return self.print_stats()
+
+    def print_help(self):
+        helptext = """\
+        Welcome to {channel_name}! I am {bot_name}, your friendly helpful workout bot. Here are a
+        couple useful commands you can use to talk with me:
+        - `{bot_name} help`: print this help message
+        - `{bot_name} user1 [user2 [...]]`: print the stats for user1, user2, ...
+        - `{bot_name} channel`: print the stats for everyone in the channel
+        - `{bot_name}, I don't have to listen to you`: doubles your exercise quota permanently (coming soon)
+        """
+        return {'text': helptext}
+
+    def print_stats(self, text):
+        words = text.split()
+        user_reverse_lookup = {}
+        users_to_print = []
+        for user_id in self.user_manager.users:
+            user = self.user_manager.users[user_id]
+            user_reverse_lookup[user.username.lower()] = user_id
+            user_reverse_lookup[user.real_name.lower()] = user_id
+        for word in words:
+            if word in user_reverse_lookup:
+                users_to_print.append(user_reverse_lookup[word])
+            elif word == "channel" or word == "@channel":
+                users_to_print = self.user_manager.users.keys()
+                break
+        if len(users_to_print) > 0:
+            stats = self.user_manager.stats(list(set(users_to_print)))
+            self.logger.info("""\
+            vvvvvvvvvvvvvvvvvvvvvvvv
+            responding:
+            {}
+            ^^^^^^^^^^^^^^^^^^^^^^^^""".format(stats))
+            return {
+                "text": stats
+            }
 
 

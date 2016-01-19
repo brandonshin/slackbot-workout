229593282cd6484252fcd8539d7a931d600f1598
Miles Yucht <mgyucht@gmail.com>
2016-01-18 22:42:45 -0800

    Fixed hopefully

--- flexbot/web.py
+++ flexbot/web.py
@@ -167,9 +167,9 @@ A little primer on how I work: after I call you out for an exercise, I will only
             for user_id in current_winners:
                 pending_exercises = current_winners[user_id]
                 exercise_strings = []
-                for exercise_name, reps in pending_exercises:
-                    self.logger.debug("Exercise: %s", exercise_name)
-                    exercise = self.user_manager.get_exercise_by_name(exercise_name)
+                for entry in pending_exercises:
+                    exercise = self.user_manager.get_exercise_by_name(entry['exercise'])
+                    reps = entry['reps']
                     exercise_string = "{} {} {}".format(reps, exercise.units, exercise.name)
                     exercise_strings.append(exercise_string)
                 response += self.user_manager.get_username(user_id).ljust(15)

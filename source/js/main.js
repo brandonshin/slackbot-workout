cc896fefeec4d9020bc9a8c18966215b6ad2af02
Grant Staublin <grant.staublin@gmail.com>
2016-03-10 17:36:03 -0500

    Lots of work on leaderboard design.

--- source/js/main.js
+++ source/js/main.js
@@ -5,6 +5,7 @@ $(function() {
 
 
 
+
   // RWD Tables
   // ====================================
   // Modified version of Foundation's RWD Tables
@@ -76,7 +77,81 @@ $(function() {
   }; // rwd-tables
 
 
+}); // Ready
 
 
 
-  }); // Ready
+
+
+
+
+
+// Modal Videos
+// ====================================
+$(function(){
+  var $triggers = $('.js-video__trigger');
+  var $closeVideo = $('#js-video__close');
+  var $body = $('body');
+  var $html = $('html');
+  var videoClass = 'video--is-playing'
+
+  $triggers.click(function(){
+    var $target = $(this);
+    var vidData = getData($target);
+    $body.addClass(videoClass);
+
+    // Disable scroll on HTML element when active
+    $html.css('overflow-y', 'hidden');
+    playVideo(vidData);
+  });
+
+  // Close modal. Stop video
+  $closeVideo.click(function(){
+    $body.removeClass(videoClass);
+    $('#js-video__element').attr('src', '');
+    $html.css('overflow-y', 'scroll');
+  });
+
+});
+
+
+// Get the data from html element
+var getData = function(target) {
+  var vidData = target.data('video');
+  var vidText = target.data('text');
+  var data = {};
+  return data = {
+    data: vidData,
+    text: vidText
+  }
+}
+
+
+ // <iframe width="560" height="315" src="https://www.youtube.com/embed/W763P0hq5nM" frameborder="0" allowfullscreen></iframe>
+
+// https://s3.amazonaws.com/gcstaublin-alyx/
+
+// Update the Src of the video
+var playVideo = function(target){
+  // var urlBase = 'https://s3.amazonaws.com/hudl-internal-assets/alyx3/';
+  var urlBase = 'https://www.youtube.com/embed/';
+  // var urlEnd = '.mov';
+  var urlEnd = '?autoplay=1;?rel=0&amp;controls=0&amp;showinfo=0';
+  var urlTarget = target.data;
+  var videoTarget = $('#js-video__element');
+  var textTarget = $('#js-video__text');
+  var videoURL = '';
+
+  if(videoURL != '') {
+    videoURL = '';
+  } else {
+      // videoURL += urlBase + urlTarget + urlEnd;
+      videoURL += urlBase + urlTarget + urlEnd;
+      videoTarget.attr('src', videoURL);
+      textTarget.text(target.text);
+  }
+  // return videoURL;
+
+}
+
+

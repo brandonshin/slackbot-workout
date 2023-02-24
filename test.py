409528c02e77547d2a1c6b933f26db6a3128426c
Zuraiz Zahoor Ajaz <59668507+Zura1z@users.noreply.github.com>
2023-02-24 22:19:41 +0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,22 @@
+import pytest
+from User import User
+import datetime
+
+@pytest.fixture
+def user():
+    return User("1234567890") # replace with a valid user ID
+
+def test_fetch_names(user):
+    assert user.username == "test_user"
+    assert user.real_name == "Test User"
+
+def test_add_exercise(user):
+    exercise = {
+        "id": 1,
+        "name": "Push-ups",
+        "units": "reps"
+    }
+    user.addExercise(exercise, 10)
+    assert user.exercises == {1: 10}
+    assert user.exercise_counts == {1: 1}
+    assert user.exercise_history == [[datetime.datetime.now().isoformat(), 1, "Push-ups", 10, "reps"]]

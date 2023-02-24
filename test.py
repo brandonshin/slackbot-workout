23c98ff6113fc9a06dee1712cc2559974b2e1723
Zuraiz Zahoor Ajaz <59668507+Zura1z@users.noreply.github.com>
2023-02-24 23:20:05 +0500

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,13 @@ def test_always_passes():
     assert True
 
 def test_user_print_statement():
-    user = User('12345')
-    assert user.real_name in str(user)
+    user = User('12345', 'testuser', 'Test User')
+    assert user.id == '12345'
+    assert user.username == 'testuser'
+    assert user.real_name == 'Test User'
+    assert user.exercise_history == []
+    assert user.exercises == {}
+    assert user.exercise_counts == {}
+    assert user.past_workouts == {}
     # assert user.username in str(user)
     # assert 'New user:' in str(user)
